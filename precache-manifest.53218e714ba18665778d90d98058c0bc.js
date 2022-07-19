8ffe35c42738664697da53399513164716eee748
yorr <1mitccc@gmail.com>
2022-07-19 18:30:37 +0530

    Updates

---
+++ precache-manifest.53218e714ba18665778d90d98058c0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c56a9fcc5ca1d3f7924",
+    "url": "/starter-project/static/css/main.d0245803.chunk.css"
+  },
+  {
+    "revision": "3c56a9fcc5ca1d3f7924",
+    "url": "/starter-project/static/js/main.a3cfaa8f.chunk.js"
+  },
+  {
+    "revision": "2c7793f3ce216ba69034",
+    "url": "/starter-project/static/js/runtime~main.e55a89d9.js"
+  },
+  {
+    "revision": "3138cd193cb99d4710ee",
+    "url": "/starter-project/static/js/2.f1380a99.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/starter-project/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "d95a1ce60bdc2a90fdc4e8d3a34739f7",
+    "url": "/starter-project/index.html"
+  }
+];
